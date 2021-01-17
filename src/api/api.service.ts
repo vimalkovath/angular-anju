3b26f0bf44487abdbac83c1acb9ad3b9f00798b2
vimal kovath <vimal.k@fulfillsolutions.com>
2021-01-18 00:29:29 +0530

    updated

---
+++ src/api/api.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ApiService {
+
+  // endpoint: string = 'https://serene-wildwood-12474.herokuapp.com';
+
+
+  endpoint: string = 'https://serene-wildwood-12474.herokuapp.com/findall';
+  // endpoint: string = 'api';
+  headers = new HttpHeaders().set('Content-Type', '*');
+
+  constructor(private http: HttpClient) { }
+
+
+  // Get all Customers
+  GetCustomers() {
+    return this.http.get(`${this.endpoint}`);
+  }
+
+
+  // Error handling
+  errorMgmt(error: HttpErrorResponse) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  }
+
+}
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import { ApiService } from './../api/api.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
+
+  CustomerData: any = [];
+  constructor(private customerApi: ApiService) {
+    this.customerApi.GetCustomers().subscribe(data => {
+      this.CustomerData = data;
+      console.log("daya");
+    })
+  }
+
+  ngOnInit() { }
+
   title = 'angular-anju';
 }
